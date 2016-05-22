8b5501e597427bd3f3052d06a42414ea6169e1ce
tlfvincent <tlfvincent@gmail.com>
2016-05-22 16:28:17 -0400

    add in_ipynb function

--- pandas_summarize/__init__.py
+++ pandas_summarize/__init__.py
@@ -3,6 +3,7 @@ from collections import Counter
 import numpy as np
 import pandas as pd
 
+
 class DataFrameSummary(object):
 
     # define global variables
@@ -22,6 +23,22 @@ class DataFrameSummary(object):
         self.count = df.count()
 
 
+    def in_ipynb():
+        '''
+        check if code is running in ipython notebook or jupyter
+        taken from SO thread:
+        http://stackoverflow.com/questions/15411967/how-can-i-check-if-code-is-executed-in-the-ipython-notebook
+        '''
+        try:
+            cfg = get_ipython().config 
+            if cfg['IPKernelApp']['parent_appname'] == 'ipython-notebook':
+                return True
+            else:
+                return False
+        except NameError:
+            return False
+
+
     def get_column_types(self):
         '''get column type of each column in dataframe'''
         types = [[col, self.df[col].dtype, 'dtypes'] for col in self.columns]
@@ -31,6 +48,17 @@ class DataFrameSummary(object):
                                              'variable'])
         return col_types_df
 
+
+    def get_unique_count(self):
+        '''get number of unique values in each column'''
+        unique_cnt = [[col,
+                       self.df[col].nunique(),
+                       'unique_count'] for col in self.columns]
+        unique_cnt_df = pd.DataFrame(unique_cnt,
+                                    columns=['column_name',
+                                             'value',
+                                             'variable'])
+        return unique_cnt_df
 
     def get_missing_count(self):
         '''get raw count of missing values in each column'''
@@ -59,9 +87,11 @@ class DataFrameSummary(object):
     def get_summary(self):
         '''collect all column summaries and return as dataframe'''
         col_types_df = self.get_column_types()
+        unique_cnt_df = self.get_unique_count()
         missing_cnt_df = self.get_missing_count()
         missing_percent_df = self.get_missing_percentage()
         df_summary = pd.concat([col_types_df,
+                                unique_cnt_df,
                                 missing_cnt_df,
                                 missing_percent_df])
         df_summary = df_summary.pivot(index='variable',
@@ -69,3 +99,19 @@ class DataFrameSummary(object):
                                       values='value')
         df_summary.index.name = None
         return df_summary
+
+
+    def get_distributions(self):
+        '''generate distribution plots and summary statistics'''
+        import matplotlib.pyplot as plt
+        plt.style.use('fivethirtyeight')
+        if in_ipynb:
+
+        else:
+
+        numeric_df = self.df._get_numeric_data()
+        f = open('/home/jack/report.html','w')
+        f.write(html_string)
+        f.close()
+
+
